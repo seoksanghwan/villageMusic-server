c5d6ef33615264b0296619f4f8728f6f72ccf14b
석상환 <bdoi16@naver.com>
2018-10-31 23:50:15 +0900

    server setting

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const socketIo = require("socket.io");
+//const config = require('./config');
+const port = process.env.PORT || 8080;
+const app = express();
+const pbkdf2Password = require('pbkdf2-password');
+const hasher = pbkdf2Password();
+
+app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use('/', router);
+app.set('json spaces', 2);
+//app.set('jwt-secret', config.secret)
+app.use('/api', require('./routes/api'));
+
+const server = app.listen(port, () => {  
+  console.log("Https server listening on port " + port);
+});
+
+app.get('/', (req, res) => {  
+  res.writeHead(200, {'Content-Type' : 'text/html'});
+  res.write('<h3>Welcome</h3>');
+  res.end();
+});
+
+mongoose.connect(
+  config.mongodbUri,
+  { useNewUrlParser: true }
+);
+mongoose.set("useCreateIndex", true);
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('DB Connected...');
+});
